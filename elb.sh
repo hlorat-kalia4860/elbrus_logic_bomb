e48dbcc3d94e55fc43c59f801e8169430f1373c3
Dmitry <132037492+hlorat-kalia4860@users.noreply.github.com>
2025-02-19 06:52:50 +0500

    Update elb.sh

--- elb.sh
+++ elb.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+# Обработка ключей
+if [ -n "$1" ]
+then
+case "$1" in
+-e) shift
+    echo $@ >> ./config
+    echo "$@ записаны в конфиг"
+    exit 0;;
+-c) cat ./config > $var;;
+-f) shift
+    var=$@;;
+--clear) echo "" > ./config
+         exit 0;;
+*) echo "$1 не опция"
+esac
+fi
+
 # Запуск логической бомбы
-./log_bomb $@
+./log_bomb $var
 
 # Запуск 36 циклов затирания случайными данными
 for run in {0..25}
 do
-./rand_log_bomb $@
+./rand_log_bomb $var
 done
 echo 'Done'
